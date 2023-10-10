5adcab7d0d93862f29dc750db79a4b10ffe60569
Vadim <hawaiianwork@mail.ru>
2023-10-10 09:43:48 +0300

    first commit

---
+++ repetition/easy.py
@@ -0,0 +1,30 @@
+a = int(input())
+b = int(input())
+
+# Сумма чисел a и b
+sm = a + b
+print(sm)
+
+# Разность чисел a и b
+diff = a - b
+print(diff)
+
+# Произведение чисел a и b
+product = a * b
+print(product)
+
+# Частное чисел a и b
+quotient = a / b
+print(quotient)
+
+# Целая часть от деления числа a на b
+integer_division = a // b
+print(integer_division)
+
+# Остаток от деления числа a на b
+remainder = a % b
+print(remainder)
+
+# Корень квадратный из суммы их 10-х степеней
+sqrt_sum_pow_10 = (a**10 + b**10)**0.5
+print(sqrt_sum_pow_10)
