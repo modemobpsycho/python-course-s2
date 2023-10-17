f2a9d2494127a8300a49a5e184f13f0b0af6ae8c
Vadim <hawaiianwork@mail.ru>
2023-10-17 20:57:17 +0300

    .

---
+++ sets/methods/example.py
@@ -0,0 +1,10 @@
+myset = set([1, 2, 2, 3, 4, 4, 4])
+print(len(myset))
+
+myset = set('ъъ эээ юююю яяяяя')
+print(len(myset))
+
+myset1 = set([1, 2, 3, 4, 5])
+myset2 = set('12345')
+
+print(myset1 == myset2)
