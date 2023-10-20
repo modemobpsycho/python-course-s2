d6f25b550e5c829a42c9e97c41632f19947fb0a7
Vadim <hawaiianwork@mail.ru>
2023-10-20 22:42:05 +0300

    up up up up up

---
+++ 6. dict/example & prefabs/prefabs.py
@@ -0,0 +1,30 @@
+languages = {'Python': 'Гвидо ван Россум', 
+             'C#': 'Андерс Хейлсберг', 
+             'Java': 'Джеймс Гослинг', 
+             'C++': 'Бьёрн Страуструп'}
+
+print('Создателем языка C# является', languages['C#']) #пример работы
+
+
+
+
+
+info = dict(name = 'Timur', age = 28, job = 'Teacher') #создание
+
+
+
+
+
+capitals = {'Россия': 'Москва', 'Франция': 'Париж', 'Чехия': 'Прага'}
+
+for key in capitals:
+    print(key)
+    
+    
+    
+
+
+capitals = {'Россия': 'Москва', 'Франция': 'Париж', 'Чехия': 'Прага'}
+
+for key in capitals:
+    print(capitals[key])
