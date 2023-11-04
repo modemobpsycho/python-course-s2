c6b0db3c3ce5740931903136db443aea0b101a13
Vadim <hawaiianwork@mail.ru>
2023-11-04 23:30:31 +0300

    updated

--- 10. functions/4. lambda-func/task8.py
+++ 10. functions/4. lambda-func/task8.py
@@ -1,12 +1,1 @@
-def func(number):
-    if (number % 19 == 0 or number % 13 == 0):
-        return True
-    else:
-        return False
-    
-    
-print(func(19))
-print(func(13))
-print(func(20))
-print(func(15))
-print(func(247))
+func = lambda x: x % 19 == 0 or x % 13 == 0
---
+++ 10. functions/4. lambda-func/task9.py
@@ -0,0 +1,1 @@
+func = lambda x: x.lower().startswith('a') and x.lower().endswith('a')
