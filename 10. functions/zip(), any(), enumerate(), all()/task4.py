6255d473fc4a26909f980b7d7c7a34b28548efc1
Vadim <hawaiianwork@mail.ru>
2023-11-08 23:56:28 +0300

    .

--- 10. functions/zip(), any(), enumerate(), all()/task4.py
+++ 10. functions/zip(), any(), enumerate(), all()/task4.py
@@ -3,4 +3,6 @@ words1 = ['яблоко', 'ананас', 'апельсин',
 words2 = ['林檎', 'パイナップル', 'オレンジ', '柿']
 words3 = ['apple', 'pineapple', 'orange', 'persimmon', 'pomegranate']
 
+
 print(len(list(zip(words1, words2, words3))))
+
