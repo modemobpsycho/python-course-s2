af5694fed2d808c6020179c76dd13c41759577ad
Vadim <hawaiianwork@mail.ru>
2023-11-10 04:05:01 +0300

    ,,

--- 10. functions/2. high-order functions/task5.py
+++ 10. functions/2. high-order functions/task5.py
@@ -1,18 +1,1 @@
-def add3(x):
-    return x + 3
-
-
-def mul7(x):
-    return x * 7
-
-
-def func_apply(function, items):
-    result = []
-    for item in items:
-        result.append(function(item))
-    return result
-
-
-print(func_apply(mul7, [1, 2, 3, 4, 5, 6]))
-print(func_apply(add3, [1, 2, 3, 4, 5, 6]))
-print(func_apply(str, [1, 2, 3, 4, 5, 6]))
+print(5 != 6)
---
+++ 10. functions/2. high-order functions/task6.py
@@ -0,0 +1,3 @@
+import numpy as np
+table = np.array([[1, 3], [2, 4]])
+print(table.max(axis = 1))
