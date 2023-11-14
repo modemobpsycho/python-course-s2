3c955b71a7b97d172d2f0ba19ad8523406a42537
Vadim <hawaiianwork@mail.ru>
2023-11-14 23:28:08 +0300

    .

--- 4. tuples/task1.py
+++ 4. tuples/task1.py
@@ -1,32 +1,39 @@
-countries = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+countries = ('Russia', 'Argentina', 'Spain',
+             'Slovakia', 'Canada', 'Slovenia', 'Italy')
 last = countries[-1]
 print(last)
 
-primes = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71) 
+primes = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+          31, 37, 41, 43, 47, 53, 59, 61, 67, 71)
 print(primes[:6])
 
-countries = ('Russia', 'Argentina', 'Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon')
+countries = ('Russia', 'Argentina', 'Slovakia', 'Canada',
+             'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon')
 print(countries[2:])
 
-countries = ('Russia', 'Argentina', 'Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon')
+countries = ('Russia', 'Argentina', 'Slovakia', 'Canada',
+             'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon')
 print(countries[2:])
 
-countries = ('Russia', 'Argentina', 'Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon')
+countries = ('Russia', 'Argentina', 'Slovakia', 'Canada',
+             'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon')
 print(countries[2:])
 
-countries = ('Romania', 'Poland', 'Estonia', 'Bulgaria', 'Slovakia', 'Slovenia', 'Hungary')
+countries = ('Romania', 'Poland', 'Estonia', 'Bulgaria',
+             'Slovakia', 'Slovenia', 'Hungary')
 number = len(countries)
 print(number)
 
 numbers = (12.5, 3.1415, 2.718, 9.8, 1.414, 1.1618, 1.324)
 print(max(numbers) + min(numbers))
 
-countries = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+countries = ('Russia', 'Argentina', 'Spain',
+             'Slovakia', 'Canada', 'Slovenia', 'Italy')
 index = countries.index("Slovenia")
 print(index)
 
 numbers1 = (1, 2, 3) * 2
-numbers2 = (6,) * 9 
+numbers2 = (6,) * 9
 numbers3 = (7, 8, 9, 10, 11, 12, 13)
 
 print(numbers1 + numbers2 + numbers3)
--- 4. tuples/task2.py
+++ 4. tuples/task2.py
@@ -1,4 +1,5 @@
-tuples = [(), (), ('',), ('a', 'b'), (), ('a', 'b', 'c'), (1,), (), (), ('d',), ('', ''), ()]
+tuples = [(), (), ('',), ('a', 'b'), (), ('a', 'b', 'c'),
+          (1,), (), (), ('d',), ('', ''), ()]
 non_empty_tuples = [i for i in tuples if len(i) > 0]
 
 print(non_empty_tuples)
