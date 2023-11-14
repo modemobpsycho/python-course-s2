d4a2cbf8a8c7785b4af34d3e23661732ceda9ddf
Vadim <hawaiianwork@mail.ru>
2023-11-14 23:47:07 +0300

    .

--- work wth files/chapter2/task7.py
+++ work wth files/chapter2/task7.py
@@ -1,7 +1,8 @@
-with open("file.txt", encoding = "utf-8") as file:
+with open("file.txt", encoding="utf-8") as file:
     text = file.read()
     len_count = len(list(filter(lambda x: x.isalpha(), text)))
-    words_count  = len(text.split())
+    words_count = len(text.split())
     lines_count = len(text.splitlines())
-    
-print(f"Input file contains:\n{len_count} letters\n{words_count} words\n {lines_count} lines")
+
+print(
+    f"Input file contains:\n{len_count} letters\n{words_count} words\n {lines_count} lines")
