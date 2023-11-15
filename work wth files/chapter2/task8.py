f266048360ec720379847ab499f81e66022b5b1e
Vadim <hawaiianwork@mail.ru>
2023-11-15 23:39:12 +0300

    .

--- work wth files/chapter2/task8.py
+++ work wth files/chapter2/task8.py
@@ -1,5 +1,8 @@
 import random
 
-with open("first_name.txt", encoding = "utf-8") as names, open("last_names.txt", encoding = "utf-8") as lastnames:
+with open("first_names.txt", encoding = "utf-8") as names, open("last_names.txt", encoding = "utf-8") as lastnames:
+    last_names = lastnames.readlines()
+    first_names = names.readlines()
     
-
+for i in range(3):
+    print(f"{random.choice(first_names).strip()} {random.choice(last_names).strip()}")
