e6df161567c792c77e7b8432fc7af524b09302dc
Vadim <hawaiianwork@mail.ru>
2023-11-15 23:40:03 +0300

    .

--- work wth files/chapter2/task9.py
+++ work wth files/chapter2/task9.py
@@ -1,3 +1,6 @@
 with open("population.txt", encoding="utf-8") as file:
     res = [line.split('\t') for line in file]
 
+for i in res:
+    if i[0].startswith('G') and int(i[1]) > 500000:
+        print(i[0])
